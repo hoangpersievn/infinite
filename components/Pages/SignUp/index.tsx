c90320f02dad26c3d5e89474c415dc8aeab88853
hoangpersievn <hoangpersievn@gmail.com>
2023-03-12 12:17:28 +0700

    Fix: Fix ci job

--- components/Pages/SignUp/index.tsx
+++ components/Pages/SignUp/index.tsx
@@ -3,18 +3,6 @@ import { Auth } from '@/components/Templates/Auth';
 import { AUTH_TYPE } from '@/constants/type';
 
 export function SignUp() {
-  const onFinish = (values: Record<string, string>) => {
-    console.log("finish", values);
-    // dispatch(showAuthLoader());
-    // dispatch(userSignIn(values));
-  };
-
-  const onFinishFailed = (
-    errorInfo: ValidateErrorEntity<Record<string, string>>
-  ) => {
-    console.log("Failed:", errorInfo);
-  };
-
   const onDispatch = ({ type, value }: { type: string; value: unknown }) => {
     console.log({ type, value });
   };
